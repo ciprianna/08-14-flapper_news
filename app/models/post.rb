b6af424989c8e399bae4cdf9890e2993eb6c8330
Ciprianna <ciprianna17@gmail.com>
2015-08-17 12:10:18 -0500

    Added a has_many relationship for posts and comments. Added a   method to include comments in the json output.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  has_many :comments
+
+  # Using the super method to return json with a post's comments
+  def as_json(options = {})
+    super(options.merge(include: :comments))
+  end
 end
